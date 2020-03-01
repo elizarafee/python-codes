bb988872d32fc072c96e49893eba9b4b3081c698
Eliza Ahmed <elizarafee@gmail.com>
2020-03-01 23:42:13 +0600

    operated various operators

---
+++ operators.py
@@ -0,0 +1,15 @@
+a = 100
+b = 50
+c = 20
+d = 10
+e = 5
+f = 2
+
+summation = a + b + c + d + e + f
+substraction = a - b - c - d - e - f
+multiplication = a * b * c * d
+devide = e / f
+modulous = e % f
+power = e ** f
+
+print(summation, substraction, multiplication, devide, modulous, power)
