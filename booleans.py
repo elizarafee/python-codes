29b78bb5886ae1263ffaeb5297c18e4d7e900b50
Eliza Ahmed <elizarafee@gmail.com>
2020-03-03 18:57:32 +0600

    Booleans and NoneType

---
+++ booleans.py
@@ -0,0 +1,13 @@
+# Booleans are operators that allow users to convey true or false statement...
+# T and F have to be capitalized in the case of writing True/False...
+
+print(1 > 2)
+print(1 == 2)
+# NoneType
+# print(b) # error will be displayed because b is not declared
+b = None                                    #NoneType
+print(b)
+a = True
+c = False
+print(a, b, c)
+print(type(a), type(b), type(c))
