866d5bc5ec357e885d924f9243202ccc12a38999
Eliza Ahmed <elizarafee@gmail.com>
2020-03-03 18:50:38 +0600

    Sets and it's attributes

---
+++ sets.py
@@ -0,0 +1,12 @@
+# Sets are unordered collections of unique elements...
+# Can only be representative of the same object...
+# Sets can't be sliced or indexed...
+my_set = {1 ,1,1, 2, 1.3, 'books'}
+print(my_set)                               # multiple 2'r wont be taken
+print(len(my_set))                          # one object is taken only one time
+my_set.add('prices')                        # adding objects
+print(my_set)
+my_list = [2, 3, 2, 3, 3]
+print(set(my_list))
+# print(my_set[1:])
+# print(my_set[0])
