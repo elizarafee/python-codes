0847d0e4c5f7d208804b286e5e21fdacbfa6b446
Eliza Ahmed <elizarafee@gmail.com>
2020-03-05 10:29:39 +0600

    python statements: IF / ELIF / ELSE

---
+++ statements.py
@@ -0,0 +1,10 @@
+# Python statements: IF / ELIF / ELSE
+
+jamai = 'Treque'
+
+if jamai == 'Tareque' or jamai == 'tareque':
+    print("Eliza's jamai <3 ")
+elif jamai == 'Tanim' or jamai =='tanim':
+    print("Tonny's jamai :) ")
+else:
+    print("Undefined!!!")
