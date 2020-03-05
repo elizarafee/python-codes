8ff4908fc25f2d833e06918abf97941876af5e64
Eliza Ahmed <elizarafee@gmail.com>
2020-03-05 10:15:35 +0600

    Chaining comparison operators

---
+++ chainingOperators.py
@@ -0,0 +1,14 @@
+# Chaining operators are: 'and / or / not'
+# NOT changes the value
+
+a = 100
+b = 80
+c = 60
+d = 40
+e = 20
+f = 10
+g = 5
+
+print(a == b and a != b)
+print(b != c or b != c)
+print(not c > d)
