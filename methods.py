27a3dc2da1f158b37d2c4a25a1efae26b97863c4
Eliza Ahmed <elizarafee@gmail.com>
2020-03-06 23:52:56 +0600

    Methods

---
+++ methods.py
@@ -0,0 +1,12 @@
+# Methods are essentially function that are built into objects
+# For getting available methods press myList.(tab)
+# Jupter notebook : for knowing more about the method myList.insert(shift + tab) 
+# without jupyter notebook : help(myList.insert)
+
+# for example
+myList = [1, 2, 3, 4]
+myList.append(5)
+myList.pop()
+
+# The secret module is used for generating cryptographycally rendom numbers suitable for managing data such as
+# password, account authentication, security token and so on
